e96dec53219eec2ec60a05291f2839445a032781
Márcio Koch <lobokoch@gmail.com>
2019-06-08 10:38:45 -0300

    Suporte a legenda

--- src/app/modules/financeiro/contaspagar/contapagar/contapagar.module.ts
+++ src/app/modules/financeiro/contaspagar/contapagar/contapagar.module.ts
@@ -1,6 +1,6 @@
 /**********************************************************************************************
-Code generated with MKL Plug-in version: 3.8.1
-Code generated at time stamp: 2019-06-08T08:28:40.433
+Code generated with MKL Plug-in version: 3.9.0
+Code generated at time stamp: 2019-06-08T10:32:58.533
 Copyright: Kerubin - logokoch@gmail.com
 
 WARNING: DO NOT CHANGE THIS CODE BECAUSE THE CHANGES WILL BE LOST IN THE NEXT CODE GENERATION.
