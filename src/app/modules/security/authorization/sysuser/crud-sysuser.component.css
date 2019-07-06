94c468b490d1d8ef508858d43fc5667097e9a8b4
Márcio Koch <lobokoch@gmail.com>
2019-07-06 11:32:38 -0300

    Ajustes nas dependências devido ao projeto service-core.

--- src/app/modules/security/authorization/sysuser/crud-sysuser.component.css
+++ src/app/modules/security/authorization/sysuser/crud-sysuser.component.css
@@ -1,6 +1,6 @@
 /**********************************************************************************************
-Code generated with MKL Plug-in version: 6.10.6
-Code generated at time stamp: 2019-07-06T07:24:18.259
+Code generated with MKL Plug-in version: 6.11.0
+Code generated at time stamp: 2019-07-06T11:24:40.670
 Copyright: Kerubin - logokoch@gmail.com
 
 WARNING: DO NOT CHANGE THIS CODE BECAUSE THE CHANGES WILL BE LOST IN THE NEXT CODE GENERATION.
